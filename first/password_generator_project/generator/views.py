1b23ed62a8e5cb89d8e6fe85c001ae16a05fa797
Anna <umilina98@mail.ru>
2023-05-29 18:10:59 +0300

    django1

--- first/password_generator_project/generator/views.py
+++ first/password_generator_project/generator/views.py
@@ -4,7 +4,7 @@ import random
 
 
 def home(request):
-    lst = list(range(6,15))
+    lst = list(range(6, 15))
     return render(request, 'generator/home.html', {'lst': lst})
 
 
@@ -25,3 +25,7 @@ def password(request):
     for i in range(length):
         psw += random.choice(char)
     return render(request, 'generator/password.html', {'password': psw})
+
+
+def information(request):
+    return render(request, 'generator/information.html', {'information': information})
--- first/password_generator_project/password_generator_project/urls.py
+++ first/password_generator_project/password_generator_project/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('password/', views.password, name='password'),
+    path('information/', views.information, name='information'),
 ]
